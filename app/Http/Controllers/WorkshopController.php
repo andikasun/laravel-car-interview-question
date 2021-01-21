916f23c067cf4e58824ff11a5428787bd49e58e9
aandika3 <aandika3@gatech.edu>
2021-01-22 02:28:10 +0800

    feat: workshop recommendation endpoint

--- app/Http/Controllers/WorkshopController.php
+++ app/Http/Controllers/WorkshopController.php
@@ -7,7 +7,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Workshop;
+use App\Appointment;
 
 class WorkshopController extends BaseController
 {
@@ -20,12 +22,37 @@ class WorkshopController extends BaseController
             $workshop->where('id', $request->input('workshop_id'));
         }
         if ($request->has('workshop_name')) {
-            $workshop->where('id', $request->input('workshop_name'));
+            $workshop->where('name', $request->input('workshop_name'));
         }
 
 
         $workshops = $workshop->with(["appointments","appointments.car"])->get();
-        return $workshops;
+        return response()->json(['status' => 'SUCCESS', "data" => $workshops], 200);
+    }
+
+    public function recommend(Request $request) {
+        $workshop = new Workshop();
+        $workshop = $workshop->newQuery();
+        $conflict = null;
+        if ($request->has('start_time') && $request->has('end_time')) {
+            $conflict = Appointment::select('workshop_id')
+                ->whereBetween('start_time', [$request->start_time, $request->end_time])
+                ->whereBetween('end_time', [$request->start_time, $request->end_time])
+                ->get();
+        }
+
+        if ($request->has('latitude') && $request->has('longitude')) {
+            $workshop->select(DB::raw('*, ST_Distance_Sphere( point(longitude, latitude), point(' . $request->longitude . ', ' . $request->latitude . ') ) as distance'))
+                ->orderBy('distance', 'ASC');
+        }
+        $workshops = $workshop->whereNotIn('id', $conflict)->get();
+
+        try {
+            return response()->json(['status' => 'SUCCESS', "data" => $workshops], 200);
+        } catch(Exception $ex) {
+            return response()->json(['status' => 'FAILED', 'error' => 'Error creating appointment'], 500);
+        }
+
     }
 }
 
