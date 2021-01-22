67af12579e888ca74fd917f52de598896ccc569e
aandika3 <aandika3@gatech.edu>
2021-01-22 15:47:15 +0800

    fix: add fillable for Appointment

--- app/Appointment.php
+++ app/Appointment.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    protected $fillable = ['car_id', 'workshop_id', 'start_time', 'end_time'];
     public function car()
     {
         return $this->belongsTo(Car::class);
