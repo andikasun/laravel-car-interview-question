97950f80f7a62fe25e0cbe5bde9553621c24c5c0
aandika3 <aandika3@gatech.edu>
2021-01-22 14:46:55 +0800

    feat: added test cases for workshop recommendation

--- tests/Feature/WorkshopTest.php
+++ tests/Feature/WorkshopTest.php
@@ -57,4 +57,55 @@ class WorkshopTest extends TestCase
                 ]
             ]);
     }
+
+    public function testWorkshopRecommend()
+    {
+        $workshopData = [
+            "start_time" => "2021-01-20 13:00:17",
+            "end_time" => "2021-01-20 14:00:17",
+            "latitude" => 1.30000,
+            "longitude" => 102.00000
+        ];
+
+        $this->json('GET', 'api/v1/recommendworkshop', $workshopData, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                "status" => "SUCCESS",
+                "data" =>[
+                    [
+                        "id"=> 2,
+                        "name" => "724 Ang Mo Kio Workshop",
+                        "phone" => "+6566668888",
+                        "latitude" => 1.3721404,
+                        "longitude" => 103.8445973,
+                        "opening_time" => "12:00",
+                        "closing_time" => "16:00",
+                        "distance" => "205210.92"
+                    ]
+            
+                ]
+            ]);
+    }
+
+    public function testWorkshopRecommendInvalidInput()
+    {
+        $workshopData = [
+            "start_time" => "2021-01-20 13:00:17",
+            "end_time" => "2021-01-20 14:00:17"
+        ];
+
+        $this->json('GET', 'api/v1/recommendworkshop', $workshopData, ['Accept' => 'application/json'])
+            ->assertStatus(500)
+            ->assertJson([
+                "status" => "FAILED",
+                "error" =>[
+                    "latitude" => [
+                        "The latitude field is required."
+                    ],
+                    "longitude" => [
+                        "The longitude field is required."
+                    ]
+                ]
+            ]);
+    }
 }
